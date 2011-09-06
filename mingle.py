56b3bea3fe21e01eb20771774b6ffa9493933061
Anisse Astier <anisse@astier.eu>
2011-09-06 22:25:25 +0200

    Implemented all remaining pipes
    Proof of concept is over. But I have doubts that changing filter order
    might just break the whole thing because of simple and double quotes
    mess.

--- mingle.py
+++ mingle.py
@@ -3,13 +3,17 @@
 def pyencode(s, codec):
     enc=s.encode(codec)
     if codec == "base64":
-        enc=enc[:-1]
+        enc=enc[:-1] #delete end with \n
     elif codec == "zlib":
+        #make sure escaping is correct (raw string)
         enc=enc.encode('string_escape')
     return '"%s"'%enc
 
 def pydecode(s, codec):
     return '%s.decode("%s")'%(s,codec)
+
+def pypipedecode(s, codec):
+    return '%s | python -c "import sys; print sys.stdin.read().decode(\'%s\')"'%(s, codec)
 
 def pyformat(s, format):
     if format == "print":
@@ -18,6 +22,10 @@ def pyformat(s, format):
         return "exec %s"%s
     elif format == "cmd":
         return "python -c '%s'"%s
+    elif format == "echone":
+        return "echo -ne %s"%s
+    elif format == "shexec":
+        return 'sh -c "$(%s)"'%s
     else:
         return format%s
 
@@ -26,32 +34,33 @@ filters = {
 "pyencode": pyencode,
 "pydecode": pydecode,
 "pyformat": pyformat,
+"pypipedecode": pypipedecode
 }
 
 
 description = [
 	["string", "Real men don't."],
-	["pyencode", "base64"], #delete end with \n
+	["pyencode", "base64"],
 	["pydecode", "base64"],
 	["pyformat", "print"],
 	["pyformat", "exec"],
-	["pyencode", "zlib"], #make sure escaping is correct (r'' string)
+	["pyencode", "zlib"],
 	["pydecode", "zlib"],
 	["pyformat", "exec"],
 	["pyformat", "cmd"],
 	["pyencode", "hex_codec"],
-	["echone", None],
+	["pyformat", "echone"],
 	["pypipedecode", "hex_codec"],
-	["shexec", None],
+	["pyformat", "shexec"],
+    ["pyencode", "hex_codec"],
 ]
 
 def main():
     s=""
     for level in description:
-        print level
         if filters.has_key(level[0]):
             s = filters[level[0]](s, level[1])
-        print s
+    print s
 
 if __name__ == "__main__":
     main()
