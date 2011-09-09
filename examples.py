7ea5428b8edca1c5c13e8004ff93313b9893bccc
Anisse Astier <anisse@astier.eu>
2011-09-09 19:51:52 +0200

    Move main and examples to a separate file for use as a library

---
+++ examples.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+
+from mingle import mingle
+
+message1 = [
+        ["string", "Mingle boggles your mind."],
+        ["pyencode", "base64"],
+        ["pyformat", '"%s"'],
+
+        ["pydecode", '"base64"'],
+
+        ["pyformat", "print"],
+        ["pyformat", "'%s'"],
+        ["pyformat", "exec"],
+
+        ["pyencode", "zlib"],
+        ["repr", None],
+        ["pydecode", '"zlib"'],
+
+        ["pyformat", "exec"],
+
+        ["escape", '"'],
+        ["shell_clean", None],
+        ["pyformat", '"%s"'],
+        ["pyformat", "cmd"],
+
+        ["pyencode", "hex_codec"],
+        ["pyformat", "echone"],
+        ["pypipedecode", "'hex_codec'"],
+        ["pyformat", "shexec"],
+        ["pyencode", "hex_codec"],
+        ]
+# Same as message1, but with base64 instead of repr (much shorter output)
+message2 = [
+        ["string", "Mingle is quite useless, too."],
+        ["pyencode", "base64"],
+        ["pyformat", '"%s"'],
+
+        ["pydecode", '"base64"'],
+
+        ["pyformat", "print"],
+        ["pyformat", "'%s'"],
+        ["pyformat", "exec"],
+
+        ["pyencode", "zlib"],
+        ["pyencode", "base64"],
+        ["pyformat", '"%s"'],
+        ["pydecode", '"base64"'],
+        ["pydecode", '"zlib"'],
+
+        ["pyformat", "exec"],
+
+        ["escape", '"'],
+        ["shell_clean", None],
+        ["pyformat", '"%s"'],
+        ["pyformat", "cmd"],
+
+        ["pyencode", "hex_codec"],
+        ["pyformat", "echone"],
+        ["pypipedecode", "'hex_codec'"],
+        ["pyformat", "shexec"],
+        ["pyencode", "hex_codec"],
+        ]
+
+# Show a message and open a webpage
+web = [
+        ["string", "https://github.com/anisse/mingle"],
+        ["pyencode", "base64"],
+        ["repr", None],
+        ["pydecode", '"base64"'],
+        ["pyformat", "webopen"],
+
+        ["pyformat", 'print "What have I done ?";%s'],
+        ["repr", None],
+        ["pyformat", "exec"],
+
+        ["pyencode", "zlib"],
+
+        ["pyencode", "base64"],
+        ["pyformat", '"%s"'],
+        ["pydecode", '"base64"'],
+
+        ["pydecode", '"zlib"'],
+
+        ["pyformat", "exec"],
+
+        ["escape", '"'],
+        ["shell_clean", None],
+        ["pyformat", '"%s"'],
+        ["pyformat", "cmd"],
+        #["pyformat", "histdisable"], #for bash debug only
+
+        ["pyencode", "base64"],
+        ["pyformat", "echone"],
+        ["pypipedecode", "'base64'"],
+        ["pyformat", "shexec"],
+        ["pyencode", "hex_codec"],
+        ]
+
+def main():
+    mingle(web, True)
+
+if __name__ == "__main__":
+    main()
--- mingle.py
+++ mingle.py
@@ -64,100 +64,6 @@ filters = {
 }
 
 
-message1 = [
-        ["string", "Mingle boggles your mind."],
-        ["pyencode", "base64"],
-        ["pyformat", '"%s"'],
-
-        ["pydecode", '"base64"'],
-
-        ["pyformat", "print"],
-        ["pyformat", "'%s'"],
-        ["pyformat", "exec"],
-
-        ["pyencode", "zlib"],
-        ["repr", None],
-        ["pydecode", '"zlib"'],
-
-        ["pyformat", "exec"],
-
-        ["escape", '"'],
-        ["shell_clean", None],
-        ["pyformat", '"%s"'],
-        ["pyformat", "cmd"],
-
-        ["pyencode", "hex_codec"],
-        ["pyformat", "echone"],
-        ["pypipedecode", "'hex_codec'"],
-        ["pyformat", "shexec"],
-        ["pyencode", "hex_codec"],
-        ]
-# Same as message1, but with base64 instead of repr (much shorter output)
-message2 = [
-        ["string", "Mingle is quite useless, too."],
-        ["pyencode", "base64"],
-        ["pyformat", '"%s"'],
-
-        ["pydecode", '"base64"'],
-
-        ["pyformat", "print"],
-        ["pyformat", "'%s'"],
-        ["pyformat", "exec"],
-
-        ["pyencode", "zlib"],
-        ["pyencode", "base64"],
-        ["pyformat", '"%s"'],
-        ["pydecode", '"base64"'],
-        ["pydecode", '"zlib"'],
-
-        ["pyformat", "exec"],
-
-        ["escape", '"'],
-        ["shell_clean", None],
-        ["pyformat", '"%s"'],
-        ["pyformat", "cmd"],
-
-        ["pyencode", "hex_codec"],
-        ["pyformat", "echone"],
-        ["pypipedecode", "'hex_codec'"],
-        ["pyformat", "shexec"],
-        ["pyencode", "hex_codec"],
-        ]
-
-# Show a message and open a webpage
-web = [
-        ["string", "https://github.com/anisse/mingle"],
-        ["pyencode", "base64"],
-        ["repr", None],
-        ["pydecode", '"base64"'],
-        ["pyformat", "webopen"],
-
-        ["pyformat", 'print "What have I done ?";%s'],
-        ["repr", None],
-        ["pyformat", "exec"],
-
-        ["pyencode", "zlib"],
-
-        ["pyencode", "base64"],
-        ["pyformat", '"%s"'],
-        ["pydecode", '"base64"'],
-
-        ["pydecode", '"zlib"'],
-
-        ["pyformat", "exec"],
-
-        ["escape", '"'],
-        ["shell_clean", None],
-        ["pyformat", '"%s"'],
-        ["pyformat", "cmd"],
-        #["pyformat", "histdisable"], #for bash debug only
-
-        ["pyencode", "base64"],
-        ["pyformat", "echone"],
-        ["pypipedecode", "'base64'"],
-        ["pyformat", "shexec"],
-        ["pyencode", "hex_codec"],
-        ]
 
 def mingle(steps, debug=False):
     s=""
@@ -168,8 +74,3 @@ def mingle(steps, debug=False):
         if debug: print "%s\n%s"%(s, len(s))
     print s
 
-def main():
-    mingle(web, True)
-
-if __name__ == "__main__":
-    main()
